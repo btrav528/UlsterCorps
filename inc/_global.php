9c7b36f18e97fcc244c6e826774148464bfb5562
Brian Travis <btrav528@gmail.com>
2014-01-24 14:45:25 -0500

    Updated to include connection to database. May change at a later date to connect to my local machine, as opposed to the school servs

--- inc/_global.php
+++ inc/_global.php
@@ -1,2 +1,9 @@
 <?php
+session_start();
+function GetConnection() {
+        global $sql_password;
+        $conn = new mysqli('localhost', 'n02207313', $sql_password, 'n02207313_db');
+        return $conn;
+
+}
 ?>
